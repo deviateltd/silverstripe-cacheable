954a7b4498098574328cd3ee081648acf1501efd
Russell Michell <russell.michell@deviate.net.nz>
2015-04-29 16:18:35 +1200

    General: - Added percentage completion logic for long-running Rebuild tasks. - Minor tidy-up, boyscout etc to  improve code readability.

--- code/tasks/CacheableNavigation_Rebuild.php
+++ code/tasks/CacheableNavigation_Rebuild.php
@@ -1,16 +1,26 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: normann.lou
- * Date: 15/03/2015
- * Time: 6:28 PM
+ * 
+ * This BuildTask pre-primes the object cache.
+ * 
+ * @author Deviate Ltd 2015 http://www.deviate.net.nz
+ * @package silverstripe-cachable
  */
-
-class CacheableNavigation_Rebuild extends BuildTask{
+class CacheableNavigation_Rebuild extends BuildTask {
+    
+    /**
+     *
+     * @var string
+     */
     protected $description = 'rebuild storage of the cacheable navigation';
 
+    /**
+     * 
+     * @param SS_HTTPRequest $request
+     * @return void
+     */
     public function run($request) {
-        $line_break = Director::is_cli()?"\n":"<br />";
+        $line_break = Director::is_cli() ? PHP_EOL : "<br />";
 
         $currentStage = Versioned::current_stage();
         $stage_mode_mapping = array(
@@ -25,29 +35,51 @@ class CacheableNavigation_Rebuild extends BuildTask{
                 Config::inst()->update("CacheableSiteConfig", 'cacheable_fields', array('SubsiteID'));
                 Config::inst()->update("CacheableSiteTree", 'cacheable_fields', array('SubsiteID'));
             }
+            
             $siteConfigs = DataObject::get('SiteConfig');
-            foreach($siteConfigs as $config){
+            foreach($siteConfigs as $config) {                
                 $service = new CacheableNavigationService($mode, $config);
                 $service->refreshCachedConfig();
+                
                 if(class_exists('Subsite')){
                     $pages = DataObject::get("Page", "\"SubsiteID\" = '".$config->SubsiteID."'");
                 }else{
                     $pages = DataObject::get("Page");
                 }
-                if($pages->exists()){
+                
+                if($pages->exists()) {
+                    $count = 0;
                     foreach($pages as $page){
+                        $count++;
                         $service->set_model($page);
+                        $percent = $this->percentageComplete($count, $pages->count());
+                        echo 'Caching: ' . $page->Title . ' (' . $percent . ')' . $line_break;
                         $service->refreshCachedPage();
                     }
                 }
+                
                 $service->completeBuild();
-                echo $pages->count()." pages being cached in $stage mode for site ".$config->ID.$line_break;
+                echo $pages->count()." pages cached in $stage mode for subsite " . $config->ID . $line_break;
             }
+            
             if(class_exists('Subsite')){
                 Subsite::disable_subsite_filter(false);
             }
         }
 
         Versioned::set_reading_mode($currentStage);
+    }
+        
+    /**
+     * 
+     * Generate a percentage of how complete the cache rebuild is.
+     * 
+     * @param number $count
+     * @param number $total
+     * @return string
+     */
+    public function percentageComplete($count, $total) {
+        $calc = (((int)$count / (int)$total) * 100);
+        return round($calc, 1) . '%';
     }
 }
